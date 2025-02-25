af65289b44ea114586901abab2cd08e068db97ed
tchau2802 <royalchau999@gmail.com>
2025-02-25 15:18:59 +0700

    setup fe - user api be

---
+++ BE/src/routes/api.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { createUser, userLogin, getAllUsers, getUser } = require('../controllers/userController');
+const delay = require('../middleware/delay');
+const jwt = require('../middleware/auth');
+
+const routerAPI = express.Router();
+
+routerAPI.all("*", jwt);
+
+routerAPI.get("/", (req, res) => {
+    return res.status(200).json("Hello API");
+})
+
+routerAPI.post("/register", createUser);
+routerAPI.post("/login", userLogin);
+routerAPI.get("/users", getAllUsers);
+routerAPI.get("/user", delay, getUser);
+
+module.exports = routerAPI
