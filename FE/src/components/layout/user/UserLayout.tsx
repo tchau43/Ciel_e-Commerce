d8ee8c705c6155ea13a480dc2273c990b697943d
Pham Thai Chau <tchau2802@gmail.com>
2025-04-22 23:04:48 +0700

    update: be - migration for database / fe - card border color - seperate navbar

--- FE/src/components/layout/user/UserLayout.tsx
+++ FE/src/components/layout/user/UserLayout.tsx
@@ -5,6 +5,7 @@ import Header from "./header/Header";
 import { Role } from "../../../types/dataTypes";
 import Topbar from "./Topbar";
 import Footer from "./Footer";
+import Navbar from "./header/Navbar";
 
 const UserLayout: React.FC = () => {
   const { token, role } = getAuthCredentials();
@@ -17,8 +18,9 @@ const UserLayout: React.FC = () => {
     <div className="flex flex-col min-h-full w-full items-center p-0 bg-ch-red-10">
       {/* <Topbar></Topbar> */}
       <Header />
+      <Navbar />
       <div className="p-4 flex bg-primary-foreground w-full flex-1 justify-center items-start">
-        <div className="min-h-full w-full max-w-9xl">
+        <div className="min-h-full w-full max-w-10xl">
           <Outlet />
         </div>
       </div>
