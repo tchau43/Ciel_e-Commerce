9f86992e28d893e2006821c1f20954c75d8e0d36
Pham Thai Chau <tchau2802@gmail.com>
2025-05-19 14:06:07 +0700

    refactor: remove unnecessary files

--- CMS/src/repositories/customerPage/customerPage.ts
+++
@@ -1,9 +0,0 @@
-import Base from "../base";
-
-class CustomerHomePage extends Base {
-  getHomePageData = async (url: string) => {
-    return this.http(url, "get");
-  };
-}
-
-export default new CustomerHomePage();
